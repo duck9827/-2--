89248ecbc35064d3ccb15f1d417152249cf33d7b
오리9827 <sspure@naver.com>
2020-12-13 03:34:18 +0900

    플레이어 이동방향에 따라 플레이어 오브젝트 회전 시도,실패.
    교수님 강의속 해당 스크립트는 agent를 이용하신듯 해서... agent를 이용하는것으로 스크립트를 고쳐보거나 다른 방법 연구 예정

---
+++ invasion/Assets/Script/Player/PlayerStatus.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerStatus : Singletorn<PlayerStatus>
+{
+    public int life = 3;
+    public int power = 1;
+    [SerializeField] private Camera cam;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "bullet")
+        {
+            life -= 1;
+
+            other.gameObject.SetActive(false);//임시로 삭제
+
+            if(life == 0)
+            {
+                print("You dead");
+            }
+
+        }
+    }
+    
+    private void Update()
+    {
+        var dir = (cam.transform.forward *
+            Input.GetAxis("Vertical") + cam.transform.right * Input.GetAxis("Horizontal")).normalized;
+        
+        
+        //보는 방향으로 플레이어 회전
+        transform.rotation = Quaternion.LookRotation(new Vector3(dir.x, 0, dir.z) * -1);
+    }
+    
+    
+    public void Init()
+    {
+        life = 3;
+        power = 1;
+
+        transform.position = Vector3.zero;
+    }
+
+    
+
+}
+
