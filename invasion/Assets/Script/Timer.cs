d2283fd382934ea72572e9a2e36de08692b664a0
오리9827 <sspure@naver.com>
2020-12-15 00:30:45 +0900

    타임어택 타이머 구현
    TimeSpan 밀리세컨드 부분에서 인스턴스 사용해서 구현시 시간 표기가 입력받은 LimitTime의 두배로 뛰는 오류가 발생하여 밀리세컨드 구현은 현재 실패한 상태입니다. 오류 해결방안을 연구해보거나 시간상 불가능할경우 밀리세컨드 부분을 없애려 합니다..ㅠㅠ

---
+++ invasion/Assets/Script/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Timer
+{
+    public class Timer : Singletorn<Timer>
+    {
+        public float LimitTime;
+        
+        private Text _text;
+
+        void Start()
+        {
+
+        }
+
+        private void Awake()
+        {
+            _text = GetComponent<Text>();
+        }
+
+        void Update()
+        {
+            LimitTime -= Time.deltaTime;
+            
+            var timeSpan = new TimeSpan(0, 0, (int)(Timer.Instance.LimitTime/60), (int)(Timer.Instance.LimitTime%60), 0);
+            _text.text = $"{timeSpan.Minutes:00}:{timeSpan.Seconds:00}.{timeSpan.Milliseconds:000}"; 
+            
+            //timeSpan 부분에 (int)(Timer.Instance.LimitTime*1000) 부분 추가시 입력받은 초가 두배로 표시되는 오류(표시상 2초씩 줄어듭니다..) 발생. 방법 연구중입니다. 
+        }
+    }
+}
+    
