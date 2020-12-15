7d4f0fb7b9842a8e858805bc08374057f9ac45ed
오리9827 <sspure@naver.com>
2020-12-15 12:58:24 +0900

    타임어택 수정&추가
    타임어택용 타이머 시간기록용으로 수정하여 추가했습니다!

---
+++ invasion/Assets/Script/TimerTextUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Timer
+{
+    public class TimerTextUI : MonoBehaviour
+    {
+        private Text _text;
+        
+        private void Awake()
+        {
+            _text = GetComponent<Text>();
+        }
+        
+        void Start()
+        {
+        
+        }
+
+
+        void Update()
+        {
+            var timeSpan = new TimeSpan(0, 0, 0, 0, (int)(Timer.Instance.CurrentTime*1000));
+            _text.text = $"{timeSpan.Minutes:00}:{timeSpan.Seconds:00}.{timeSpan.Milliseconds:000}"; 
+        }
+    }  
+}
+
