282e583490fbcb4159f132d611550e5f6cc24a92
오리9827 <sspure@naver.com>
2020-12-15 17:24:25 +0900

    카메라전환 구현
    보스패턴2 시작시 탑뷰로, 그 외의 상황에서는 3인칭뷰로 되도록 카메라 전환 구현하였습니다.

--- invasion/Assets/Script/Boss/BossPattern2.cs
+++ invasion/Assets/Script/Boss/BossPattern2.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class BossPattern2 : Singletorn<BossPattern2>
 {
     public GameObject bullet;//임시
-
+    public static bool topview = false;     //보스패턴 시작시 true로 전환 (탑뷰 카메라 전환을 위함)
+    
+    //[SerializeField] private CameraManager _cameraManager;
     [SerializeField] float patroll = 10f;
     [SerializeField] float patrollDelay = 5f;
     private bool active = false;
     private Vector3 d;
 
+
     private void Update()
     {
-        if(active)
+        if (active)
+        {
             transform.position = Vector3.Lerp(transform.position, d, 0.005f);
+            topview = true;     //카메라가 탑뷰로 전환됨 (CameraManager스크립트에서 처리)
+        }
 
+        if (active == false)     //만약 보스 패턴이 시작되거나 끝나지 않았을경우 3인칭뷰로 전환합니다.
+        {
+            topview = false;
+        }
     }
 
     public void StartPattern()
---
+++ invasion/Assets/Script/CameraManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera ThirdPersonCamera;
+    public Camera TopViewCamera;
+    //private BossPattern2 call = GameObject.Find("Top View Camera").GetComponent < Update > ();
+    
+    private void Start()     //시작할땐 3인칭 카메라를 켜둡니다. 
+    {
+        ThirdPersonView();
+    }
+
+    public void TopView()     //시점을 탑뷰로 전환
+    {
+        TopViewCamera.enabled = true;
+        ThirdPersonCamera.enabled = false;
+    }
+
+    public void ThirdPersonView()     //시점을 3인칭으로 변환
+    {
+        TopViewCamera.enabled = false;
+        ThirdPersonCamera.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (BossPattern2.topview == true)     //top뷰로 전환
+        {
+            TopView();
+        }
+        if (BossPattern2.topview == false)     //3인칭뷰로 전환
+        {
+            ThirdPersonView();
+        }
+    }
+    //private void Update()
+    //{
+        //if (Input.GetKey("1"))     //1번키를 누르면 탑뷰로 전환
+        //{
+            //TopView();
+        //}
+
+        //if (Input.GetKey("2"))     //2번키를 누르면 3인칭 뷰로 전환 
+        //{
+            //ThirdPersonView();
+        //}
+    //}
+}
