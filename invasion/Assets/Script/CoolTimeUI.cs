3f0e10c4504d218d517a845ce661cd549278dd97
오리9827 <sspure@naver.com>
2020-12-17 03:27:43 +0900

    스킬 지속시간 및 쿨타임구현
    무적스킬의 지속시간은 PlayerController 스크립트의 skilltime 변수, 쿨타임은 CoolTimeUI 스크립트의 cooltime 변수입니다.

--- invasion/Assets/Script/CoolTimeUI.cs
+++ invasion/Assets/Script/CoolTimeUI.cs
@@ -1,30 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
  
-public class CoolTimeUI : MonoBehaviour {
- 
+public class CoolTimeUI : MonoBehaviour
+{
+    public static float cooltime = 10.0f;     //무적상태 쿨타임을 PlayerController 스크립트에서 처리 / 쿨타임 15초
     public Image img_Skill;
-    
-    void Start () {
-        
-    }
-    
+    private bool check;      //쿨타임 UI표시 체크 변수(false시에만 쿨타임 재생 / 초기값은 false)
+
     void Update () {
-        if (Input.GetKeyDown (KeyCode.Space))
+        if (Input.GetKeyDown((KeyCode.Space)))  //코루틴이 실행되지 않던 동안에만 이게 실행되게 해야...? 
         {
-            StartCoroutine(CoolTime (15.0f));
+            if (check == false)
+            {
+                StartCoroutine(CoolTime (cooltime));
+            }
         }
     }
- 
+    
     IEnumerator CoolTime (float cool)
     {
+        check = true;
         while (cool > 1.0f)
         {
             cool -= Time.deltaTime;
             img_Skill.fillAmount = (1.0f / cool);
             yield return new WaitForFixedUpdate();
         }
+        check = false;
     }
 }
--- invasion/Assets/Script/Player/PlayerController.cs
+++ invasion/Assets/Script/Player/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Camera cam;
     [SerializeField] private float speed = 5f;
     
+    public static float skilltime = 3.0f;     //스킬 시간
+    private float skill_timer; //스킬 지속여부 체크용 변수
+    private bool check;      //스킬 쿨타임 여부 체크
+    private bool skillCheck; //스킬사용여부 체크, false일땐 사용한 상태입니다. 
+    private float timer; //쿨타임 여부 체크
+
     private NavMeshAgent _agent; //스테이지 네비게이션 bake 필요
     
     private void Awake()
@@ -18,6 +24,8 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        print(skill_timer);
+        //print(timer);
         var dir = (cam.transform.forward *
             Input.GetAxis("Vertical") + cam.transform.right * Input.GetAxis("Horizontal")).normalized;
         
@@ -38,9 +46,19 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space)) //스페이스바를 누를 시 무적
         {
-            PlayerStatus.noDamage = true;     //무적상태로 변경
+            if ((skillCheck == false)&&(timer <= 0.0f))
+            {
+                StartCoroutine("NoDamage");
+                StartCoroutine("SkillCool");
+            }
         }
 
+    }
+
+    private void Start()
+    {
+        skill_timer = skilltime;
+        timer = 0.0f;
     }
 
     IEnumerator Shot()
@@ -53,4 +71,32 @@ public class PlayerController : MonoBehaviour
             yield return new WaitForSeconds(0.2f);
         }
     }
+    
+    IEnumerator NoDamage ()
+    {
+        skillCheck = true;
+        while (skill_timer > 0)
+        {
+            PlayerStatus.noDamage = true;     //무적상태로 변경
+            skill_timer -= Time.deltaTime;
+            timer -= Time.deltaTime; 
+            yield return new WaitForSeconds(0.0f);
+        }
+        skillCheck = false;
+        PlayerStatus.noDamage = false;
+        skill_timer = skilltime;
+    }
+    
+    IEnumerator SkillCool ()
+    {
+        check = true;
+        while (timer <= (CoolTimeUI.cooltime-4.0f))
+        {
+            timer += Time.deltaTime;
+            yield return new WaitForSeconds(0.0f);
+        }
+        check = false;
+        timer = 0.0f;
+    }
+    
 }
