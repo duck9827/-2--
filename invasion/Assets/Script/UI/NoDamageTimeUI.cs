9669f091dcbe3c9730b4aabcb0132d0e493cb4a5
오리9827 <sspure@naver.com>
2020-12-17 04:22:48 +0900

    스킬 관련 UI추가 및 기타 UI 수정
    스킬 지속시간 관련 UI추가(슬라이더바 이용) 및 전반적인 UI 수정하였습니다.

---
+++ invasion/Assets/Script/UI/NoDamageTimeUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoDamageTimeUI : MonoBehaviour
+{
+    private Slider skillTImer;
+    void Start()
+    {
+        skillTImer = GetComponent<Slider>();
+        skillTImer.value = PlayerController.skilltime + 1.5f;
+    }
+    
+    void Update()
+    {
+        print(skillTImer.value);
+        if ((skillTImer.value >= 0.0f)&&(PlayerStatus.noDamage == true))
+        {
+            skillTImer.value -= Time.deltaTime;     //스킬시간만큼 슬라이더가 줄어듭니다. 
+        }
+        else if (PlayerStatus.noDamage == false)     //스킬을 다 쓰고 나면 다시 초기화 
+        {
+            skillTImer.value = PlayerController.skilltime = 1.5f;
+        }
+    }
+}
