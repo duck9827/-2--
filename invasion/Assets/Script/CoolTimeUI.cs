164e571711a5735a8a7ab884e0431226dbfe21e0
오리9827 <sspure@naver.com>
2020-12-16 22:10:36 +0900

    보스&플레이어 기체 변경 및 쿨타임 UI추가
    보스랑 플레이어 기체 변경 및 쿨타임 UI 추가, 쿨타임 기능 누락으로 재추가 및 UI와 연동 예정입니다.

--- invasion/Assets/Script/CoolTimeUI.cs
+++ invasion/Assets/Script/CoolTimeUI.cs
@@ -2,40 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class CoolTimeUI : MonoBehaviour
-{
-    public Image image;
-    public Button button;
-    public float coolTime = 10.0f;
-    public bool isCool = true;
-    private float leftTime = 10.0f;
-    private float speed = 5.0f;
-    void Update()
-    {
-        if (isCool)
+ 
+public class CoolTimeUI : MonoBehaviour {
+ 
+    public Image img_Skill;
+    
+    void Start () {
+        
+    }
+    
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.Space))
         {
-            if (leftTime > 0)
-            {
-                leftTime -= Time.deltaTime * speed;
-                if(leftTime < 0) {
-                    leftTime = 0;
-                    if(button)
-                        button.enabled = true;
-                    isCool = true;   
-                }
-
-                float ratio = 1.0f - (leftTime / coolTime);
-                if(image)
-                    image.fillAmount = ratio;
-            }
+            StartCoroutine(CoolTime (15.0f));
         }
     }
-
-    public void StartCoolTime() {
-        leftTime = coolTime;
-        isCool = true;
-        if(button)
-            button.enabled = false; // 버튼 기능을 해지함.
+ 
+    IEnumerator CoolTime (float cool)
+    {
+        while (cool > 1.0f)
+        {
+            cool -= Time.deltaTime;
+            img_Skill.fillAmount = (1.0f / cool);
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
