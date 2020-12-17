3fbfd99eb9226946fce1abb6362570720d6183bf
오리9827 <sspure@naver.com>
2020-12-17 14:32:21 +0900

    스카이박스 회전+변경
    스카이박스 변경하고 자동으로 회전하도록 스크립트 추가하였습니다!

---
+++ invasion/Assets/Script/MoveSky.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSky : MonoBehaviour
+{
+    float degree;
+
+    void Start ()
+    {
+        degree = 0;
+    }
+	
+    void Update ()
+    {
+        degree += Time.deltaTime;
+        if (degree >= 360)
+            degree = 0;
+
+        RenderSettings.skybox.SetFloat("_Rotation", degree);
+    }
+}
